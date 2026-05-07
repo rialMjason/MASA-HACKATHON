baed41465bd148739bbe8b84f515e0ed5538ebf4
Jason Choong Jin Shen <jasonchoongjinshen123@gmail.com>
2026-05-07 04:51:58 +0000

    Use uniform flag styling for all countries (remove per-country focus class)

--- dashboard/app.js
+++ dashboard/app.js
@@ -55,18 +55,11 @@ let currentFetchRequest = null;
 function createFlagIcon(country) {
     // Use flagUrl if available, otherwise use emoji
     if (country.flagUrl) {
-        const focusClassMap = {
-            MY: 'flag-focus-my',
-            SG: 'flag-focus-sg',
-            TL: 'flag-focus-tl'
-        };
-        const focusClass = focusClassMap[country.code] || '';
-
         return L.divIcon({
             className: 'flag-marker',
             html: `<div class="flag-ball">
                         <div class="flag-image-container">
-                            <img src="${country.flagUrl}" alt="${country.name} flag" class="flag-image ${focusClass}" onerror="this.parentElement.innerHTML='<div class=\\\"flag-emoji\\\">${country.flagEmoji || '🏴'}</div>'">
+                            <img src="${country.flagUrl}" alt="${country.name} flag" class="flag-image" onerror="this.parentElement.innerHTML='<div class=\\"flag-emoji\\">${country.flagEmoji || '🏴'}</div>'">
                         </div>
                         <div class="ball-shine"></div>
                     </div>`,
